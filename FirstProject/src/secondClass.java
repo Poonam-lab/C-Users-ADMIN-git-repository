4a580edc1e3dd25d61e0479bbb099f96336a458f
Poonam Nishad <poonamnishad23.2001@gmail.com>
2024-10-30 11:29:55 +0530

    first push of the code

---
+++ FirstProject/src/secondClass.java
@@ -0,0 +1,17 @@
+
+public class secondClass {
+	
+	static	void show()
+	{
+		System.out.println("In second class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+		
+	
+
+}
